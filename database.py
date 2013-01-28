c50abe16223e0fb97c0aa9fddb0ba58b0df25a34
Nathan Crandall <cbcramps@gmail.com>
2013-01-28 00:11:30 -0800

    global template values

--- database.py
+++ database.py
@@ -28,6 +28,10 @@ from google.appengine.ext import db
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 def render_template(handler_object, file_name, template_values):
+  user = users.get_current_user()
+  template_values['user'] = user
+  template_values['logout_url'] = users.create_logout_url()
+  template_values['login_url'] = users.create_login_url(handler_object.request.uri)
   template = jinja_environment.get_template(file_name)
   handler_object.response.out.write(template.render(template_values))
   
