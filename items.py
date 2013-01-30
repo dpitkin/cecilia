110e58138697da8e909f84ad00d1a9b590e82491
Nathan Crandall <cbcramps@gmail.com>
2013-01-29 23:58:00 -0800

    logging started

--- items.py
+++ items.py
@@ -11,23 +11,31 @@ class MainHandler(database.webapp2.RequestHandler):
 
 class NewHandler(database.webapp2.RequestHandler):
   def get(self):
-    database.render_template(self, 'items/new_item.html', {})
+    if database.users.get_current_user():
+      database.render_template(self, 'items/new_item.html', {})
+    else:
+      self.redirect('/')
     
 class ViewHandler(database.webapp2.RequestHandler):
   def get(self):
     item = db.get(db.Key.from_path('Item', int(self.request.get('item_id'))))
-    database.render_template(self, 'items/view_item.html', {'item': item})
+    li = db.GqlQuery("SELECT * FROM LoginInformation WHERE user_id = :1", item.created_by_id).get()
+    database.render_template(self, 'items/view_item.html', {'item': item, 'li': li})
     
 class SaveHandler(database.webapp2.RequestHandler):
   def post(self):
     user = database.users.get_current_user()
-    item = database.Item()
-    item.title = cgi.escape(self.request.get('title'))
-    item.description = cgi.escape(self.request.get('description'))
-    item.price = '%.2f' % float(cgi.escape(self.request.get('price')))
-    item.created_by_id = user.user_id()
-    item.put()
-    self.redirect('/items/')
+    if user:
+      item = database.Item()
+      item.title = cgi.escape(self.request.get('title'))
+      item.description = cgi.escape(self.request.get('description'))
+      item.price = '%.2f' % float(cgi.escape(self.request.get('price')))
+      item.created_by_id = user.user_id()
+      item.put()
+      database.logging.info("Created a new item.\nTitle: %s\nDescription: %s\nPrice: %s\nCreatedBy: %s", item.title, item.description, item.price, item.created_by_id)
+      self.redirect('/items/')
+    else:
+      self.redirect('/')
     
 class DeleteHandler(database.webapp2.RequestHandler):
   def get(self):
@@ -37,6 +45,7 @@ class DeleteHandler(database.webapp2.RequestHandler):
       item = db.get(db.Key.from_path('Item', int(self.request.get('item_id'))))
       #make sure the person owns this item
       if item.created_by_id == user.user_id():
+        database.logging.info("Deleting item with id %s", item.key().id())
         database.db.delete(item)
         redirect = '/items/my_items'
     self.redirect(redirect)
@@ -58,6 +67,7 @@ class UpdateHandler(database.webapp2.RequestHandler):
       item.title = cgi.escape(self.request.get('title'))
       item.description = cgi.escape(self.request.get('description'))
       item.price = cgi.escape(self.request.get('price'))
+      database.logging.info("Item #%s changed to:\nTitle: %s\nDescription: %s\nPrice: %s", item.key().id(), item.title, item.description, item.price)
       item.put()
       self.redirect('/items/my_items')
     else:
