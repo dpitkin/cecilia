2677498ecdf65bf5c20e0ff4384d44b335ef0eb9
Dane Pitkin <depitkin@cox.net>
2013-02-19 21:11:33 -0800

    Only use get_current_li().is_admin when checking admin privileges

--- database.py
+++ database.py
@@ -39,13 +39,14 @@ def render_template(handler_object, file_name, template_values):
   user = users.get_current_user()
   if user:
     current_li = db.GqlQuery("SELECT * FROM LoginInformation WHERE user_id = :1", user.user_id()).get()
+    template_values['is_admin'] = current_li.is_admin
   else:
     current_li = None
+    template_values['is_admin'] = users.is_current_user_admin()
   template_values['current_li'] = current_li
   template_values['user'] = user
   template_values['logout_url'] = users.create_logout_url('/')
   template_values['login_url'] = users.create_login_url('/users/verify_user/')
-  template_values['is_admin'] = current_li.is_admin
   if user:
     li = db.GqlQuery("SELECT * FROM LoginInformation WHERE user_id = :1", user.user_id()).get()
     template_values['unread_messages'] = db.GqlQuery("SELECT * FROM Message WHERE recipient_id = :1 AND read = :2", user.user_id(), False).count()
--- main.py
+++ main.py
@@ -22,7 +22,8 @@ cgi = database.cgi
 
 class MainHandler(database.webapp2.RequestHandler):
   def get(self):
-    if database.get_current_li().is_admin:
+
+    if database.get_current_li() and database.get_current_li().is_admin:
       items = database.db.GqlQuery("SELECT * FROM Item")
     else:
       items = database.db.GqlQuery("SELECT * FROM Item WHERE expiration_date >= :1 AND is_active = :2", database.datetime.date.today(), True)
